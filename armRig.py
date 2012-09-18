69590edf04fc30167b6257621f983f0e69479160
zeth willie <zethwillie@gmail.com>
2012-09-18 10:32:14 -0400

    adjusted for fixed baselimb method pull outs
    callLocators() became setupArgs() and callLocatorsGo()

--- armRig.py
+++ armRig.py
@@ -41,46 +41,15 @@ class ArmUI(LimbUI):
 		self.widgets["customFrame"] = cmds.frameLayout(l="3. Custom Limb Attributes", w=320, bgc=(0,0,0), p=self.widgets["mainColumn"])
 		cmds.text("custom arms stuff here, like hand, etc")
 
-	def callLocators(self,*args):
-		#set up the variables to pass to the LimbClass?
-		self.limbName = cmds.textFieldGrp(self.widgets["limbNameTFG"], q=True, tx=True) #string
-		self.prefixRaw = cmds.radioButtonGrp(self.widgets["prefixRBG"], q=True, sl=True ) #string
-		if self.prefixRaw == 1:
-			self.prefix = ""
-		elif self.prefixRaw == 2:
-			self.prefix = "lf"
-		elif self.prefixRaw == 3:
-			self.prefix = "rt"
-		elif self.prefixRaw == 4:
-			self.prefix = cmds.textFieldGrp(self.widgets["otherPrefixTFG"], q=True, tx=True)
-		self.mirror = cmds.checkBox(self.widgets["mirrorCB"],q=True, v=True)#bool
-		self.mirrorAxisRaw = cmds.radioButtonGrp(self.widgets["axisRBG"], q=True, sl=True)#string
-		if self.mirrorAxisRaw == 1:
-			self.mirrorAxis = "yz"
-		elif self.mirrorAxisRaw ==2:
-			self.mirrorAxis = "xz"
-		elif self.mirrorAxisRaw ==3:
-			self.mirrorAxis = "xy"
-		self.jointSuffix = cmds.textFieldGrp(self.widgets["jntSuffixTFG"], q=True, tx=True) #string
-		self.controlSuffix = cmds.textFieldGrp(self.widgets["ctrlSuffixTFG"], q=True, tx=True) #string
-		self.groupSuffix = cmds.textFieldGrp(self.widgets["groupSuffixTFG"], q=True, tx=True) #string
-		self.spreadTwist = cmds.checkBoxGrp(self.widgets["jointOptionsCBG"], q=True, v1=True)#bool
-		self.spreadTwistLow = cmds.checkBoxGrp(self.widgets["jointOptionsCBG"], q=True, v3=True)#bool
-		self.stretchy = cmds.checkBoxGrp(self.widgets["jointOptionsCBG"], q=True, v2=True) #bool
-		self.numSpreadJnts = cmds.intFieldGrp(self.widgets["numSpreadIFG"], q=True, v=True) #int
-		self.locVals = self.defaultLocValues
-		self.jointList = self.defaultJointList
-		self.noFlip = cmds.checkBox(self.widgets["noFlipCB"], q=True, v=True )
-		self.mainAxis = cmds.radioButtonGrp(self.widgets["jntMainAxisRBG"], q=True, sl=True)
-		self.secondaryAxis = cmds.radioButtonGrp(self.widgets["jntSecondAxisRBG"], q=True, sl=True)
-		self.straightLimb = cmds.checkBox(self.widgets["straightLimbCB"], q=True, v=True)
-		self.mirrorName = cmds.textFieldGrp(self.widgets["otherMirrorTFG"], q=True, tx=True)
-		self.posNeg = cmds.radioButtonGrp(self.widgets["jntSecondAxisDirRBG"], q=True, sl=True)
+	def setupArgs(self,*args):
+		super(ArmUI, self).setupArgs()
+		#add things here from UI for hand stuff
 
-######### pull the below bit out into separate function in baseLimb to just override that????
-
+	def callLocatorsGo(self, *args):
 		#now pass these values to the Limb class - create an instance of Limb
 		self.arms = Arm(self.limbName, self.prefix, self.mirror, self.mirrorAxis, self.jointSuffix, self.controlSuffix, self.groupSuffix, self.spreadTwist, self.spreadTwistLow, self.stretchy, self.numSpreadJnts, self.locVals, self.jointList, self.noFlip, self.mainAxis, self.secondaryAxis, self.straightLimb, self.mirrorName, self.posNeg)
+
+		#do I need to add stuff here for hand? or can I just deal with it below in the hand class
 
 		#now create locators
 		self.arms.createLocators()
@@ -94,10 +63,31 @@ class Arm(Limb):
 		print("got the super: %s, %s"%(limbName, prefix))
 		print("and added some stuff to the init (hence this messgae)")
 
-	# def setupIK(self, x):
-		#pull out the bits from teh main main IK code into methods that create the control and do shit to it. This way I can just overwrite that bit
+	# def setupIK(self, x, IKHandle):
+		#pass
 
-	#create hand (big job!)
+
+
+
+
+#create hand (big job!) separate class? Get variables here for myself (don't do them above?)
+
+#locs in basic hand shape (query how many fingers)
+
+#create joint structure for basic hand shape (create such that "cup" attr works correctly)
+
+#create finger joints
+
+#for finger rig, use multiple groups (one for adjustments, one for stretch(?), one for sdks, one for attr controls and one master for placement (all above the controls))
+
+#finger stretch? or just use the FK controls to move things (parent constrain to the controls, not orient) 
+#if I do that then I can also use a "stretch" attr that moves the controls(!) not the joints
+
+#leave a place for IK finger controls. . . 
+
+#finally, hook the hand into the bind rig wrist control
+
+
 
 
 
