5ed594c8fc2bbdf6085357e88ec1811982505b89
zeth willie <zethwillie@gmail.com>
2018-06-14 14:04:51 -0400

    through stretchIk setup

--- rigger_tools/BaseLimb.py
+++ rigger_tools/BaseLimb.py
@@ -3,6 +3,7 @@ import python_rigger.rigger_tools.rigger_tools as zrt
 reload(zrt)
 import zTools.rig.zbw_rig as rig
 reload(rig)
+import maya.OpenMaya as om
 
 # make a class for ctrls? ? so we can get groups with .attrs ?
 class BaseLimbUI(object):
@@ -21,6 +22,8 @@ class BaseLimb(object):
         self.origFkJnts = []
         self.mirrorFkJnts = []
         
+        # turn on decompose matrix plugin
+
         # delete this
         self.set_values_UI()
         # this will be what the actual code is. We'll just set all the values from the UI in the other case
@@ -54,6 +57,7 @@ class BaseLimb(object):
         self.ikJoints = {}
         self.ikCtrls = {}
         self.ikCtrlGrps = {}
+        self.ikHandles = {}
         self.measureJoints = {}
         self.deformJoints = {}
         self.switchCtrls = {}
@@ -66,15 +70,20 @@ class BaseLimb(object):
 
     def set_values_code(self):
         """set all relevant values from code"""
-        pass
-
     # add pts here as args instead of in init ( this is so we can call this from code)
     # add basenames here instead of in init
     # clean up options for wrist (instead of just zyx, make it more general)
+        pass
+
+
     def pose_initial_joints(self):
     # get and pass values for orient order and uporient axis
     # put joint in ref display layer temporarily
         self.joints = zrt.create_joint_chain(self.pts, self.baseNames, self.orientOrder, self.upOrientAxis)
+        # put tmp joints in a display layer and set to reference
+        cmds.select(self.joints, r=True)
+        self.dl = cmds.createDisplayLayer(name="tmp_{0}_jnt_DL".format(self.part))
+        cmds.setAttr("{0}.displayType".format(self.dl), 2)
 
         self.poseCtrls, self.poseGrps, self.octrls, self.ogrps = zrt.create_controls_and_orients_at_joints(self.joints[:-1], "sphere", self.primaryAxis, "poseCTRL", orient=True, upAxis="y")
         lockAttrs = ["s","tx", "ty", "tz"]
@@ -92,8 +101,11 @@ class BaseLimb(object):
                 cmds.setAttr("{0}.ry".format(ctrlHierList[i][0]), l=True)
                 cmds.setAttr("{0}.rz".format(ctrlHierList[i][0]), l=True)
                 cmds.setAttr("{0}.r{1}".format(ctrlHierList[i][0], self.upAxis), l=False)
-
-            cmds.setAttr("{0}.t{1}".format(ctrlHierList[i][0], self.primaryAxis), l=False)
+            if i==0:
+                cmds.setAttr("{0}.tx".format(ctrlHierList[i][0]), l=False)
+                cmds.setAttr("{0}.ty".format(ctrlHierList[i][0]), l=False)
+                cmds.setAttr("{0}.tz".format(ctrlHierList[i][0]), l=False)
+            # cmds.setAttr("{0}.t{1}".format(ctrlHierList[i][0], self.primaryAxis), l=False)
 
             oc1 = cmds.orientConstraint(self.joints[i], ctrlHierList[i][4], mo=False)
             cmds.delete(oc1)
@@ -113,8 +125,11 @@ class BaseLimb(object):
             self.mirror_joints()
         self.setup_dictionaries()        
         self.create_duplicate_chains()
-        self.setup_fk_rig()
+        self.create_fk_rig()
         self.create_fkik_switch()
+        self.connect_deform_joints()
+        self.create_ik_rig()
+        self.create_ik_stretch()
 
     def create_initial_components(self):
         # create groups and hierarchy
@@ -123,13 +138,12 @@ class BaseLimb(object):
 
     def clean_initial_joints(self):
 
-    #take joints out of display layer and delete display layer
+        # delete display layer
+        cmds.delete(self.dl)
 
-        #delete all constraints from the pose
         cmds.delete(self.poseConstraints)
         # unlock octrl attrs
         attrs = ["t", "rx", "ry", "rz"]
-
         for i in range(len(self.octrls)):
             for attr in attrs:
                 cmds.setAttr("{0}.{1}".format(self.octrls[i], attr), l=False)
@@ -167,38 +181,39 @@ class BaseLimb(object):
         self.fkCtrls["orig"] = None
         self.fkCtrlGrps["orig"] = None
         self.switchCtrls["orig"] = None
+        self.ikCtrls["orig"] = None
+        self.ikCtrlGrps["orig"] = None
+        # self.measureDist["orig"] = None
         if self.mirror:
             self.side["mir"] = self.mirPrefix
             self.fkJoints["mir"] = self.mirrorFkJnts
             self.fkCtrls["mir"] = None
             self.fkCtrlGrps["mir"] = None
             self.switchCtrls["mir"] = None
-
+            self.ikCtrls["mir"] = None # [ikCtrl, pvCtrl, pvLine]
+            self.ikCtrlGrps["mir"] = None  # [ikCtrlGrp, pvCtrlGrp, pvLIne]
+            # self.measureDist["mir"] = None
 
     def create_duplicate_chains(self):
         # for key in jntDict, make duplicates. . . (put into dictionary)
         for side in self.fkJoints.keys():
             topJnt = self.fkJoints[side][0] 
-            #self.jntDict[side]["fk"][0]
-            
+          
             # make deform jnts
             deforms = zrt.duplicate_and_rename_chain(topJnt, "deform")
             self.deformJoints[side] = deforms
-            # self.jntDict[side]["deform"] = deforms   
             
             # if we're making IK stuff
             if self.createIK:
                 # make IK
                 iks = zrt.duplicate_and_rename_chain(topJnt, "IK")
                 self.ikJoints[side] = iks
-                # self.jntDict[side]["ik"] = iks
                 # make measure
                 measures = zrt.duplicate_and_rename_chain(topJnt, "measure")
                 self.measureJoints[side] = measures
-                # self.jntDict[side]["measure"] = measures
 
 
-    def setup_fk_rig(self):
+    def create_fk_rig(self):
 
         for side in self.fkJoints.keys():
             fkJoints = self.fkJoints[side]
@@ -210,7 +225,6 @@ class BaseLimb(object):
                 grps.append(grp)
             zrt.parent_hierarchy_grouped_controls(ctrls, grps)
 
-            #add to dictionaries
             self.fkCtrls[side] = ctrls
             self.fkCtrlGrps[side] = grps
 
@@ -218,25 +232,26 @@ class BaseLimb(object):
             for i in range(len(fkJoints)):
                 pc = cmds.parentConstraint(ctrls[i], fkJoints[i])
                 sc = cmds.scaleConstraint(ctrls[i], fkJoints[i])
+        
+        # add gimble ctrls? just a group and control above each ctrl 
         # create group for arm to go in
 
 
     def create_fkik_switch(self):
         for side in self.deformJoints.keys():
             # this defaults to the third joint in chain
-            targetJnt = self.deformJoints[side][2]
-            ctrl, grp = zrt.create_control_at_joint([targetJnt], "star", self.primaryAxis, "{0}_{1}_IkFkSwitch_CTRL".format(self.side[side], self.part))
-
-            # offset it to the side (based on scale from FK wrist ctrl)
-            #get scale factor
-            amt = -5
-            if self.tertiaryAxis=="x":
-                mv=(amt, 0 , 0)
-            if self.tertiaryAxis=="y":
-                mv=(0, amt, 0)
-            else:
-                mv=(0, 0, amt)
-            cmds.xform(grp, r=True, ws=True, t=mv)
+            ctrl = rig.createControl(name="{0}_{1}_IkFkSwitch_CTRL".format(self.side[side], self.part), type="star", axis=self.primaryAxis)
+            grp = rig.groupFreeze(ctrl)
+        #get scale factor
+            root = cmds.xform(self.deformJoints[side][0], q=True, ws=True, rp=True)
+            mid = cmds.xform(self.deformJoints[side][1], q=True, ws=True, rp=True)
+            end = cmds.xform(self.deformJoints[side][2], q=True, ws=True, rp=True)
+            distVec = om.MVector(mid[0]-end[0], mid[1]-end[1], mid[2]-end[2])
+            dist = distVec.length()
+            mv = zrt.get_planar_position(root, mid, end, percent=0.05, dist=dist)
+            rig.snapTo(self.deformJoints[side][2], grp)
+            cmds.xform(grp, ws=True, t=(mv.x, mv.y, mv.z))
+            
             rig.stripTransforms(ctrl)
             cmds.addAttr(ctrl, ln="fkik", at="float", min=0.0, max=1.0, defaultValue=0.0, keyable=True)
 
@@ -246,21 +261,92 @@ class BaseLimb(object):
             self.switchCtrls[side] = ctrl
 
 
-    def build_ik_rig(self):
-        pass
+    def create_ik_rig(self):
+        for side in self.ikJoints.keys():
+            jnts = self.ikJoints[side]
+            if side == "orig":
+                name = "{0}_{1}_IK".format(self.origPrefix, self.part)
+            elif side == "mir":
+                name = "{0}_{1}_IK".format(self.mirPrefix, self.part)
+
+            handle = cmds.ikHandle(startJoint=jnts[0], endEffector=jnts[2], name=name, solver="ikRPsolver")[0]
+            cmds.setAttr("{0}.visibility".format(handle), 0)
+            ctrl, grp = zrt.create_control_at_joint(jnts[2], "arrowCross", self.primaryAxis, "{0}_CTRL".format(name))
+            cmds.parent(handle, ctrl)
+            oc = cmds.orientConstraint(ctrl, jnts[2], mo=True)
+            self.ikCtrls[side] = [ctrl]
+            self.ikCtrlGrps[side] = [grp] 
+        # scale the control
+
+            # create pole vec
+            if side == "orig":
+                pvname = "{0}_{1}_poleVector_CTRL".format(self.origPrefix, self.part)
+            elif side == "mir":
+                pvname = "{0}_{1}_poleVector_CTRL".format(self.mirPrefix, self.part)
+            pv = rig.createControl(name=pvname, type="sphere", color="red", axis="x")
+            self.ikCtrls[side].append(pv)
+            pvgrp = rig.groupFreeze(pv)
+            self.ikCtrlGrps[side].append(pvgrp)
+
+            # place and constrain pole vec
+            cmds.select(handle, r=True)
+            pos = zrt.find_pole_vector_location(handle)
+            cmds.xform(pvgrp, ws=True, t=(pos[0], pos[1], pos[2]))
+            pvc = cmds.poleVectorConstraint(pv, handle)
+        # save constraint?
+            # add crv lines from shoulder to pv
+            if side == "orig":
+                pvline = "{0}_{1}_poleVec_Line".format(self.origPrefix, self.part)
+            elif side == "mir":
+                pvline = "{0}_{1}_poleVec_Line".format(self.mirPrefix, self.part)        
+            pvLine = zrt.create_line_between(pv, jnts[1], "{0}".format(pvline))
+            self.ikCtrls[side].append(pvLine)
+            # set line as reference override
+            shp = cmds.listRelatives(pvLine, s=True)[0]
+            cmds.setAttr("{0}.overrideEnabled".format(shp), 1)
+            cmds.setAttr("{0}.overrideDisplayType".format(shp), 2)
+            #connect to switch
+            cmds.addAttr(self.switchCtrls[side], ln="poleVecLine", at="short", min=0, max=1, dv=1, k=True)
+            cmds.connectAttr("{0}.poleVecLine".format(self.switchCtrls[side]), "{0}.overrideVisibility".format(shp))
 
 
     def connect_deform_joints(self):
-        pass
-    # build IK ctrls - put into ik dict key (pv and ik ctrl)
-    # set up IK - should it be translate? 
+        # zip up fk, ik and deform joints for easier stuff
+        for side in self.fkJoints.keys():
+            joints = zip(self.fkJoints[side], self.ikJoints[side], self.deformJoints[side])
+            # should we parent constraint these? 
+            for grp in joints:
+                zrt.create_parent_reverse_network(grp[:-1], grp[-1], "{0}.fkik".format(self.switchCtrls[side]), index=0)
+
+    # a way to do gimbles in both ik and fk? create gimbel ctrl under grp. grp is parent constrained to ik/fk ctrls (like deform joint). ctrl then is what parent constrains to the deform joints
+
+    def create_ik_stretch(self):
+        # LET"S DO THIS SCALE-WISE FOR NOW
+        for side in self.ikJoints.keys():
+            if side == "orig":
+                sideName = self.origPrefix
+            if side == "mir":
+                sideName = self.mirPrefix
+
+            self.upMult, self.loMult = zrt.create_stretch_setup(self.measureJoints[side], self.ikCtrls[side][0], "{0}_{1}".format(sideName, self.part))
+        # should do twist stuff first? Need to add twist joints to ik? or just have a separate twist dict?
+            cmds.connectAttr("{0}.output.outputX".format(self.upMult), "{0}.sx".format(self.ikJoints[side][0]))
+            cmds.connectAttr("{0}.output.outputX".format(self.loMult), "{0}.sx".format(self.ikJoints[side][1]))
+
+
     # setup twist attributes - connect them
+        # turn off scale compensate on all the twist joints? 
+
+    # mult stretch stuff by all ik joints
+
+    # add option for switch ctrl to drive the rot orders of each part (all the ctrls and joints)
 
     # option for ribbon setup in here? 
 
     #set up blends for all self.joints (traslate, rotate, scale)
 
-    # package up components
+    # package up components, color controls
 
     # bind joints for QSS (different if ribbon or just twist)
     # add option for game exportable joints QSS 
+    # QSS for all controls
--- rigger_tools/rigger_tools.py
+++ rigger_tools/rigger_tools.py
@@ -1,7 +1,9 @@
 import maya.cmds as cmds
 import zTools.rig.zbw_rig as rig
+reload(rig)
+import maya.OpenMaya as om
 
-# start w joint chain
+
 def create_joint_chain(ptList=None, baseNames=None, alongAxis="xyz", upAxis="yup"):
     cmds.select(cl=True)
     jntList = []
@@ -13,7 +15,6 @@ def create_joint_chain(ptList=None, baseNames=None, alongAxis="xyz", upAxis="yup
 
     return(jntList)
 
-# create controls and parent under joint - should these go into the FK dict key?
 
 def orient_joint_to_transform(jnt, obj):
 
@@ -32,7 +33,6 @@ def orient_joint_to_transform(jnt, obj):
     cmds.parent(newChildren, jnt)
 
 
-# orient the joints (freeze transforms, then orient via options)
 def orient_joint_chain(joint=None, alongAxis="xyz", upAxis="yup"):
     cmds.joint(joint, e=True, orientJoint=alongAxis, children=True, secondaryAxisOrient=upAxis, zso=True )
 
@@ -52,8 +52,6 @@ def get_chain_hierarchy(topJoint=None):
     return(chain)
 
 
-# mirror stuff
-# mirror joint chains across given axis, with given prefixes. . .  
 def mirror_joint_chain(topJoint, oldSidePrefix=None, newSidePrefix=None, axis="yz"):
     """axis is mirror plane (xy, xz, yz)"""
     mirrored_chain = []
@@ -66,7 +64,6 @@ def mirror_joint_chain(topJoint, oldSidePrefix=None, newSidePrefix=None, axis="y
     return(mirrored_chain)
 
 
-# this is temp simple version to just replace FK
 def duplicate_and_rename_chain(topJnt, chain):
     """chain is string name of this chain (i.e. 'IK', 'measure', etc)"""
     dupe = cmds.duplicate(topJnt, renameChildren=True)
@@ -97,9 +94,41 @@ def create_blend_network(name, oneAttr, twoAttr, blendAttr, targetAttr):
     return(blend)
 
 
+def create_orient_reverse_network(srcList, tgt, switchAttr, index=0):
+    """
+    create orient constraint to A, B, which is reversed
+    """
+    oc = cmds.orientConstraint(srcList, tgt, mo=False)[0]
+    if index == 0:
+        cmds.connectAttr(switchAttr, "{0}.w1".format(oc))
+        create_reverse_network(tgt, switchAttr, "x", "{0}.w0".format(oc))
+    elif index == 1:
+        cmds.connectAttr(switchAttr, "{0}.w0".format(oc))
+        create_reverse_network(tgt, switchAttr, "x", "{0}.w1".format(oc))
+    return(oc)
+
+
+def create_parent_reverse_network(srcList, tgt, switchAttr, index=0):
+    """
+    create parent constraint to A, B, which is reversed
+    """
+    pc = cmds.parentConstraint(srcList, tgt, mo=False)[0]
+    if index == 0:
+        cmds.connectAttr(switchAttr, "{0}.w1".format(pc))
+        create_reverse_network(tgt, switchAttr, "x", "{0}.w0".format(pc))
+    elif index == 1:
+        cmds.connectAttr(switchAttr, "{0}.w0".format(pc))
+        create_reverse_network(tgt, switchAttr, "x", "{0}.w1".format(pc))
+    return(pc)
+
+
+def create_scale_reverse_network():
+    pass
+
+
 def create_reverse_network(name,  inputAttr, revAttr, targetAttr):
     """revAttr should be 'all', 'x', 'y' or 'z'"""
-    reverse = cmds.shadingNode("reverse", asUtility=True, name=name)
+    reverse = cmds.shadingNode("reverse", asUtility=True, name=name+"_reverse")
     if revAttr=="all":
         cmds.connectAttr(inputAttr, "{0}.input".format(reverse))
         cmds.connectAttr("{0}.output".format(reverse), targetAttr)
@@ -112,13 +141,16 @@ def create_reverse_network(name,  inputAttr, revAttr, targetAttr):
     if revAttr=="z":
         cmds.connectAttr(inputAttr, "{0}.input.inputZ".format(reverse))
         cmds.connectAttr("{0}.output.outputZ".format(reverse), targetAttr)
+    return(reverse)
 
 
-# DO THIS FOR ONE JNT AT A TIME SO WE CAN NAME THIS
 def create_control_at_joint(jnt, ctrlType, axis, name):
     """orient will create a new control UNDER the ctrl that we can use to  orient pose joints"""
     ctrl = rig.createControl(name, ctrlType, axis)
     grp = rig.groupFreeze(ctrl)
+    rotOrder = cmds.xform(jnt, q=True, roo=True)
+    cmds.xform(ctrl, roo=rotOrder)
+    cmds.xform(grp, roo=rotOrder)
     rig.snapTo(jnt, grp)
 
     return(ctrl, grp)
@@ -131,8 +163,6 @@ def create_controls_and_orients_at_joints(jntList, ctrlType, axis, suffix, orien
     octrls = []
     ogrps = []
     for jnt in jntList:
-        # pos = cmds.xform(jnt, q=True, ws=True, rp=True)
-        # rot = cmds.xform(jnt, q=True, ws=True, ro=True)
         if "_" in jnt:
             name = "_".join(jnt.split("_")[:-1]) + "_{0}".format(suffix)
             oname = "_".join(jnt.split("_")[:-1]) + "_{0}".format("ORIENT"+suffix)
@@ -160,9 +190,10 @@ def create_controls_and_orients_at_joints(jntList, ctrlType, axis, suffix, orien
     else:
         return(ctrls, groups, octrls, ogrps)
 
+
 def parent_hierarchy_grouped_controls(ctrls, grps):
     """assumes in order from top to bottom, groups are parents of controls"""
-    if not len(ctrls)==len(grps) or len(ctrls)<1:
+    if not len(ctrls)==len(grps) or len(ctrls)<2:
         cmds.warning("riggerTools.parent_hierarchy_grouped_controls: lists don't match in length or aren't long enough")
         return()
 
@@ -170,15 +201,169 @@ def parent_hierarchy_grouped_controls(ctrls, grps):
         cmds.parent(grps[x], ctrls[x-1])
 
 
-# create Fk controls
-# # create control, group freeze, snap to, contrain to . . . 
+# SEPARATE THIS INTO TWO FUNCTIONS (ONE FOR PV SPECIFICALLY, THE OTHER TO JUST FIND THE PLANE)
+def find_pole_vector_location(handle):
+    # arg is ikHandle
+    ikJntList = cmds.ikHandle(handle, q=True, jointList=True)
+    ikJntList.append(cmds.listRelatives(ikJntList[-1], children=True, type="joint")[0])
 
-# create control hierarchy (from group oriented controls)
+    # get jnt positions
+    rootPos = cmds.xform(ikJntList[0], q=True, ws=True, rp=True)
+    midPos = cmds.xform(ikJntList[1], q=True, ws=True, rp=True)
+    endPos = cmds.xform(ikJntList[2], q=True, ws=True, rp=True)
 
-# create Ik controls 
+    poleVecPos = get_planar_position(rootPos, midPos, endPos)
 
-# create fkIK switch
-# hook blends and controls vis into swtich
+    return(poleVecPos) 
+    
+
+def get_planar_position(rootPos, midPos, endPos, percent=None, dist=None):
+    # convert to vectors
+    rootVec = om.MVector(rootPos[0], rootPos[1], rootPos[2])
+    midVec = om.MVector(midPos[0], midPos[1], midPos[2])
+    endVec = om.MVector(endPos[0], endPos[1], endPos[2])
+    
+    # get vectors
+    line = (endVec - rootVec)
+    point = (midVec - rootVec)
+
+    # get center-ish of rootEnd (relative to midjoint)
+    if not percent:
+        percent = (line*point)/(line*line)
+    projVec = line * percent + rootVec
+    
+    if not dist:
+        dist = (midVec-rootVec).length() + (endVec-midVec).length()
+    poleVecPos = (midVec - projVec).normal() * dist + midVec
+    
+    return(poleVecPos)
+
+
+def create_line_between(startXform, endXform, name):
+    pos1 = [0,0,0]
+    pos2 = [1,1,1]
+    crv = cmds.curve(d=1, p=[pos1, pos2])
+    crv = cmds.rename(crv, "{0}_CRV".format(name))
+
+    dm1 = cmds.createNode("decomposeMatrix", name="{0}0_DM".format(name))
+    dm2 = cmds.createNode("decomposeMatrix", name="{0}1_DM".format(name))
+
+    cmds.connectAttr("{0}.worldMatrix".format(startXform), "{0}.inputMatrix".format(dm1))
+    cmds.connectAttr("{0}.worldMatrix".format(endXform), "{0}.inputMatrix".format(dm2))
+
+    cmds.connectAttr("{0}.outputTranslate".format(dm1), "{0}.controlPoints[0]".format(crv))
+    cmds.connectAttr("{0}.outputTranslate".format(dm2), "{0}.controlPoints[1]".format(crv))
+
+    return(crv)
+
+def measure_chain_length(chainList, name):
+    
+    num = len(chainList)
+    distNodes = []
+    dmNodes = []
+    for i in range(len(chainList)-1):
+        if i==0:
+            dm0 = cmds.createNode("decomposeMatrix", name="{0}{1}_0_DM".format(name, i))
+            dmNodes.append(dm0)
+            dm1 = cmds.createNode("decomposeMatrix", name="{0}{1}_1_DM".format(name, i))
+            dmNodes.append(dm1)
+        else:
+            dm0 = dmNodes[-1]
+            dm1 = cmds.createNode("decomposeMatrix", name="{0}{1}_0_DM".format(name, i))
+            dmNodes.append(dm1)
+            
+        db = cmds.createNode("distanceBetween", name="{0}{1}_DB".format(name, i))
+        
+        if i==0:
+            cmds.connectAttr("{0}.worldMatrix".format(chainList[i]), "{0}.inputMatrix".format(dm0))
+        cmds.connectAttr("{0}.worldMatrix".format(chainList[i+1]), "{0}.inputMatrix".format(dm1))
+        cmds.connectAttr("{0}.outputTranslate".format(dm0), "{0}.point1".format(db))
+        cmds.connectAttr("{0}.outputTranslate".format(dm1), "{0}.point2".format(db))        
+
+        distNodes.append(db)
+        
+    add = cmds.shadingNode("plusMinusAverage", asUtility=True, name="{0}_PMA".format(name))
+    for i in range(len(distNodes)):
+        cmds.connectAttr("{0}.distance".format(distNodes[i]), "{0}.input1D[{1}]".format(add, i))
+    
+    # returns the plusMinusAvg node, and the distance nodes
+    return(add, distNodes)
+
+
+def create_stretch_setup(measureJnts, ikCtrl, limbName):
+    """
+    note: returs only the mult nodes that you need to hook up to your joints
+    """
+    # get measure distances (x2)
+    msrAdd, msrDists = measure_chain_length(measureJnts, "{0}_measure".format(limbName))
+    # get ik to measure shoulder distance
+    ikAdd, ikDist = measure_chain_length([measureJnts[0], ikCtrl], "{0}_ik".format(limbName))
+
+    # check if ikCtrl has necessary attrs
+    divider = cmds.attributeQuery("__stretch__", node=ikCtrl, exists=True)    
+    upScale = cmds.attributeQuery("upScale", node=ikCtrl, exists=True)
+    loScale = cmds.attributeQuery("loScale", node=ikCtrl, exists=True)
+    autostretch = cmds.attributeQuery("autostretch", node=ikCtrl, exists=True)    
+    
+    if not divider:
+        cmds.addAttr(ikCtrl, sn="__stretch__", at="enum", enumName="------", k=True)
+        cmds.setAttr("{0}.__stretch__".format(ikCtrl), l=True)
+    if not upScale:
+        cmds.addAttr(ikCtrl, ln="upScale", at="float", dv=1, min=0.1, max=3, k=True)
+    if not loScale:
+        cmds.addAttr(ikCtrl, ln="loScale", at="float", dv=1, min=0.1, max=3, k=True)
+    if not autostretch:
+        cmds.addAttr(ikCtrl, ln="autostretch", at="float", dv=1, min=0, max=1.0, k=True)
+
+    # create mult for up and down limb
+    upMult1 = cmds.shadingNode("multiplyDivide", asUtility=True, name="{0}_up1_mult".format(limbName))
+    loMult1 = cmds.shadingNode("multiplyDivide", asUtility=True, name="{0}_lo1_mult".format(limbName))
+    
+    # connect distance up/down to mults, connect ik ctrl up/down attrs to mults
+    cmds.connectAttr("{0}.distance".format(msrDists[0]), "{0}.input1X".format(upMult1))
+    cmds.connectAttr("{0}.distance".format(msrDists[1]), "{0}.input1X".format(loMult1))
+    cmds.connectAttr("{0}.upScale".format(ikCtrl), "{0}.input2X".format(upMult1))
+    cmds.connectAttr("{0}.loScale".format(ikCtrl), "{0}.input2X".format(loMult1))
+    
+    # connect mults to addNode (up/down)
+    cmds.connectAttr("{0}.outputX".format(upMult1), "{0}.input1D[0]".format(msrAdd), f=True)
+    cmds.connectAttr("{0}.outputX".format(loMult1), "{0}.input1D[1]".format(msrAdd), f=True)
+
+    # create blend color, connect ikCtrl.autostretch to blender, add nodes out to blend.x (x2)
+    asBlend = cmds.shadingNode("blendColors", asUtility=True, name="{0}_autostretchBlend".format(limbName))
+    cmds.connectAttr("{0}.autostretch".format(ikCtrl), "{0}.blender".format(asBlend))
+    cmds.connectAttr("{0}.output1D".format(msrAdd), "{0}.color2.color2R".format(asBlend))
+    cmds.connectAttr("{0}.output1D".format(ikAdd), "{0}.color1.color1R".format(asBlend))
+
+    # create multdiv ratio,blend.out to ratio1.x,up/down measure add to ratio2.x
+    ratio = cmds.shadingNode("multiplyDivide", asUtility=True, name="{0}_ratio_mult".format(limbName))
+    cmds.setAttr("{0}.operation".format(ratio), 2)
+    cmds.connectAttr("{0}.output.outputR".format(asBlend), "{0}.input1.input1X".format(ratio))
+    cmds.connectAttr("{0}.output1D".format(msrAdd), "{0}.input2.input2X".format(ratio))
+
+    # create condition node
+    # ratio out x to cond first term, color if true
+    # second term=1, color if Flase = 1
+    cond = cmds.shadingNode("condition", asUtility=True, name="{0}_stretchCond".format(limbName))
+    cmds.connectAttr("{0}.output.outputX".format(ratio), "{0}.firstTerm".format(cond))
+    cmds.connectAttr("{0}.output.outputX".format(ratio), "{0}.colorIfTrue.colorIfTrueR".format(cond))
+    cmds.setAttr("{0}.operation".format(cond), 2)
+    cmds.setAttr("{0}.secondTerm".format(cond), 1)
+    cmds.setAttr("{0}.colorIfFalse.colorIfFalseR".format(cond), 1)
+
+    # create mult for up/down limbs
+    upMult2 = cmds.shadingNode("multiplyDivide", asUtility=True, name="{0}_up2_mult".format(limbName))
+    loMult2 = cmds.shadingNode("multiplyDivide", asUtility=True, name="{0}_lo2_mult".format(limbName))
+    # out color of cond to mults 1x,up/down scale from ikCtrl to mult2
+    cmds.connectAttr("{0}.outColor.outColorR".format(cond), "{0}.input1.input1X".format(upMult2))
+    cmds.connectAttr("{0}.outColor.outColorR".format(cond), "{0}.input1.input1X".format(loMult2))
+    cmds.connectAttr("{0}.upScale".format(ikCtrl), "{0}.input2.input2X".format(upMult2))
+    cmds.connectAttr("{0}.loScale".format(ikCtrl), "{0}.input2.input2X".format(loMult2))
+
+    # returns 2 mults go to the up/down jnts
+    return(upMult2, loMult2)
+
+# hook controls vis into swtich
 
 # setup stretch (measure jtns, ik jnts, ik ctrl)
 
